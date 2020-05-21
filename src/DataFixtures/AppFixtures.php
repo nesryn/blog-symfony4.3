5e9169b107e3f56e2684621fa54c59b635870521
nesrine essaies <nesrineessaies@gmail.com>
2020-05-21 02:55:19 +0100

    add fake data to tables & add attribut comments tou BlogPost

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\Comment;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -15,9 +16,14 @@ class AppFixtures extends Fixture
      */
     private  $passwordEncoder;
 
+    /**
+     * @var \Faker\Factory
+     */
+    private $faker;
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncoder= $passwordEncoder;
+        $this->faker = \Faker\Factory::create();
     }
 
     /**
@@ -28,39 +34,65 @@ class AppFixtures extends Fixture
     {
     $this->loadUsers($manager);
     $this->loadBlogPosts($manager);
+    $this->loadComments($manager);
 
     }
 
     public function loadBlogPosts(ObjectManager $manager)
     {
-        $user=$this->getReference('user_admin');
-        $blogPost =new BlogPost();
-        $blogPost->setTitle("first post")
-            ->setPublished(new \DateTime('now'))
-            ->setContent("content")
-            ->setSlug("slug")
-            ->setAuthor($user);
-        $manager->persist($blogPost);
+
+        for ($i =0; $i<=50 ;$i++){
+            $randrefUser = rand(0,25);
+
+            $blogPost =new BlogPost();
+            $blogPost->setTitle($this->faker->realText(30))
+                ->setPublished(new \DateTime('now'))
+                ->setContent($this->faker->realText())
+                ->setSlug($this->faker->slug)
+                ->setAuthor($this->getReference("user_admin_$randrefUser"));
+            $this->setReference("blog_post_$i",$blogPost);
+            $manager->persist($blogPost);
+        }
+
         $manager->flush();
     }
 
     public function loadComments(ObjectManager $manager)
     {
+        for ($i=0; $i<=50 ; $i++)
+        {
+            for ($j=0; $j< rand(1,3); $j++){
+                $randrefUser = rand(0,25);
+                $comment= new Comment();
+                $comment
+                    ->setContent($this->faker->realText())
+                    ->setPublished($this->faker->dateTimeThisYear)
+                    ->setBlogPost($this->getReference("blog_post_$i"))
+                    ->setAuthor($this->getReference("user_admin_$randrefUser"));
+                $manager->persist($comment);
+            }
+
+        }
+        $manager->flush();
 
     }
 
     public function loadUsers(ObjectManager $manager)
     {
-        $user =new User();
-        $user->setUsername('admin')
-            ->setName('name')
-            ->setEmail('email@email.com')
-            ->setPassword($this->passwordEncoder->encodePassword(
-                $user,'admin')
-            )
-            ;
-        $this->addReference('user_admin',$user);
-        $manager->persist($user);
+      for ($i=0; $i<= 25 ;$i++)
+      {
+          $user =new User();
+          $user->setUsername($this->faker->realText(10))
+              ->setName($this->faker->realText(15))
+              ->setEmail($this->faker->email)
+              ->setPassword($this->passwordEncoder->encodePassword(
+                  $user,'admin')
+              )
+          ;
+          $this->addReference("user_admin_$i",$user);
+          $manager->persist($user);
+      }
+
         $manager->flush();
     }
 
--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\BlogPostRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -48,6 +50,16 @@ class BlogPost
      * @ORM\JoinColumn(nullable=false)
      */
     private $author;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="blogPost", orphanRemoval=true)
+     */
+    private $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -111,6 +123,37 @@ class BlogPost
     public function setAuthor(?User $author): self
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setBlogPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getBlogPost() === $this) {
+                $comment->setBlogPost(null);
+            }
+        }
 
         return $this;
     }
